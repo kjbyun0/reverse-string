eb4382b2e5dece1dc0512d163e93c2f950d0b941
KI JONG BYUN <kjbyun0@gmail.com>
2024-09-06 16:29:16 -0500

    Add my solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
   // type your code here
+  let reverse = "";
+  for (const ch of str) {
+    reverse = ch + reverse;
+  }
+  return reverse;
 }
 
 if (require.main === module) {
@@ -11,6 +16,22 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+  console.log("Expecting: ''");
+  console.log("=>", reverseString(""));
+
+  console.log("");
+  console.log("Expecting: 'a'");
+  console.log("=>", reverseString("a"));
+
+  console.log("");
+  console.log("Expecting: 'ba'");
+  console.log("=>", reverseString("ab"));
+
+  console.log("");
+  console.log("Expecting: 'cba'");
+  console.log("=>", reverseString("abc"));
 }
 
 module.exports = reverseString;
